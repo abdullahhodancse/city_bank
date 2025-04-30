2349a7d1165ac44862cad34d654c2f9fd7978fa4
Abdullah Hodan <abdullahhodan448@gmail.com>
2025-04-30 23:25:09 +0600

    Initial commit

--- bank/settings.py
+++ bank/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 # import dj_database_url
 import environ
 
@@ -19,7 +20,8 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
-environ.Env.read_env()
+# environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Your secret key
 
 SECRET_KEY=env("SECRET_KEY")
