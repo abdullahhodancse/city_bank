7564142e4bc03c931955aa64f368ad03ffb1213e
Abdullah Hodan <abdullahhodan448@gmail.com>
2025-07-09 20:32:52 +0600

    Trusted add

--- bank/settings.py
+++ bank/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY=env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://city-bank-5.onrender.com/','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://city-bank-5.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
